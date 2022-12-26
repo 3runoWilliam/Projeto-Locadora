1ad76c72cee09e5bdbe55314fdfff8e92d19eaa5
Bruno William <85655579+3runoWilliam@users.noreply.github.com>
2022-12-26 14:40:55 -0300

    Update Projeto_Locadora_BD.sql

--- Projeto_Locadora_BD.sql
+++ Projeto_Locadora_BD.sql
@@ -8,20 +8,12 @@ endereco TEXT
 
 INSERT INTO Cliente(name, idade, telefone, endereco) VALUES ('Bruno William', 20, 84988248208, 'Rua Candido Martins, 1130')
 
-SELECT * FROM Cliente
-
 CREATE TABLE Aluguel(
 Data_aluguel TEXT,
 Data_entrega TEXT,
 valor_aluguel INT,
 FK_Cliente FOREIGN, 
 FK_DVD FOREIGN
-)
-
-CREATE TABLE DVD(
-Id_DVD SERIAL PRIMARY KEY,
-FK_Filme FOREIGN,
-Qnt_DVD TEXT	
 )
 
 CREATE TABLE Filme(
@@ -34,5 +26,3 @@ CREATE TABLE Dono(
 Id_Dono INT PRIMARY KEY, 
 senha TEXT PRIMARY KEY
 )
-
-
