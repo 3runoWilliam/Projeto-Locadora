b27b4a6b85d28355d150cc3dd1b1363ddc5a61b8
Bruno William <85655579+3runoWilliam@users.noreply.github.com>
2023-05-26 21:47:08 -0300

    Update Projeto_Locadora_BD.sql

--- Projeto_Locadora_BD.sql
+++ Projeto_Locadora_BD.sql
@@ -27,3 +27,23 @@ Id_Dono INT PRIMARY KEY,
 senha TEXT NOT NULL,
 nome TEXT
 )
+
+SELECT data_aluguel, data_entrega, valor_aluguel, fk_filme FROM cliente,aluguel WHERE id = fk_cliente AND id = 17
+
+ALTER TABLE aluguel ADD id_aluguel SERIAL PRIMARY KEY 
+
+ALTER TABLE aluguel ADD fk_filme INT
+
+ALTER TABLE aluguel ADD FOREIGN KEY (fk_filme) REFERENCES filme(id_filme)
+
+ALTER TABLE aluguel ADD fk_cliente INT
+
+ALTER TABLE aluguel ADD FOREIGN KEY (fk_cliente) REFERENCES cliente(id)
+
+SELECT * FROM clientes
+
+SELECT * FROM aluguel
+
+SELECT * FROM filme
+
+SELECT * FROM cliente
